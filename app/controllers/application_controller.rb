59cfea968cb06713334559275a2a4937190b94c1
Jonathan Norman <jnorman2123@gmail.com>
2019-11-22 23:28:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,14 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
 
+    erb :index
+  end
+
+  get '/recipes/new' do
+
+    erb :new
+  end
 end
